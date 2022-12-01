cbed23da842b3c6c8b6c7b6b8301655786170b65
Zach Durham <zdurham101@gmail.com>
2022-12-01 17:28:05 -0500

    solved day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,28 +1,23 @@
-use std::fs;
-
-fn read_file() -> Vec<&'static str> {
-    let file_path = "./src/day1/input.txt";
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let array_of_calories = contents.lines().map(|s| s.trim().map(String::from)).collect::<Vec<String>>()
-}
-
 pub fn day1() {
-    let mut most_calories = 0;
-    let mut current_elf_calories = 0;
-    let elf_calories = read_file();
-    for calories in elf_calories {
-         match calories {
-            "" => {
-                if current_elf_calories > most_calories {
-                    most_calories = current_elf_calories;
-                    current_elf_calories = 0;
-                }
-            },
-            _ => {
-                current_elf_calories += calories.parse::<i32>().expect("whoa there not a number")
-            }
+    let string = include_str!("./input.txt");
+    let mut totals: Vec<i32> = vec![];
+    let mut total: i32 = 0;
+    
+    let split = string.split("\n");
+    for x in split {
+        let x = x.parse::<i32>();
+        if x.is_ok() {
+            total += x.unwrap();
+        } else {
+            totals.push(total);
+            total = 0;
         }
     }
 
-    println!("{}", most_calories)
+    println!("{}", totals.iter().max().unwrap());
+
+    // part 2
+    totals.sort();
+    totals.reverse();
+    println!("{}", totals[0..3].iter().sum::<i32>());
 }
