b234bc03d746b04a06a2802e14cbf020deff3e6c
Zach Durham <zdurham101@gmail.com>
2022-12-01 13:13:34 -0500

    incorrect so far but working on it

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,14 +1,28 @@
-use std::env;
 use std::fs;
 
-fn read_file() {
+fn read_file() -> Vec<&'static str> {
     let file_path = "./src/day1/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-
-    println!("With text:\n {contents}")
+    let array_of_calories = contents.lines().map(|s| s.trim().map(String::from)).collect::<Vec<String>>()
 }
 
 pub fn day1() {
-    read_file()
+    let mut most_calories = 0;
+    let mut current_elf_calories = 0;
+    let elf_calories = read_file();
+    for calories in elf_calories {
+         match calories {
+            "" => {
+                if current_elf_calories > most_calories {
+                    most_calories = current_elf_calories;
+                    current_elf_calories = 0;
+                }
+            },
+            _ => {
+                current_elf_calories += calories.parse::<i32>().expect("whoa there not a number")
+            }
+        }
+    }
+
+    println!("{}", most_calories)
 }
